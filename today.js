fcd452a3019eeec4008abc88e1f71e158367f06a
Antonykibet <antonykibet059@gmail.com>
2023-01-15 23:53:25 +0300

    1/15/23

--- today.js
+++ today.js
@@ -53,7 +53,28 @@ for(key in salaries){
     sum+=salaries[key];
 }
 console.log(sum);
-*/
+
+Multiply numeric property values by 2
+Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+
+For instance:
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// after the call
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};
+Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
 
 let menu = {
     width: 200,
@@ -71,3 +92,67 @@ function multiplyNumeric(menu){
 multiplyNumeric(menu);
 console.log(menu.width);
 console.log(menu.height);
+
+
+const user={
+    name:"antony",
+    age:20,
+};
+let a=user;
+a.name="Eugene";
+console.log(user.name);
+
+const user={
+    name:"antony",
+    age:20,
+};
+let clone=object.assign({},user);
+console.log(clone.name);
+//We also can use Object.assign to perform a simple object cloning:
+
+const user={
+    name:"antony",
+    age:20,
+};
+let clone={};
+Object.assign(clone,user);
+console.log(clone.name);
+
+//Deep cloning,,,when clonning objects with an object in them as a property, youll inly clone the object's refrence and not its property that is why you have to deep clone.
+
+const user={
+    name:"antony",
+    age:20,
+    object2:{
+        value1:50
+    }
+};
+let clone=structuredClone(user);
+clone.object2.value1=70;
+console.log(clone.object2.value1);//70
+console.log(user.object2.value1)//50
+
+let user={
+    name:"Antony",
+    age:20,
+    sayHi(){
+        return "hello";
+    },
+}
+console.log(user.sayHi());
+
+let user={
+    name:"Antony",
+    age:20,
+    sayHi(){
+        return this.name;
+    },
+}
+console.log("hello "+user.sayHi());
+*/
+let user={
+    name:"Antony",
+    "what he likes":"painting",
+}
+let container="what he likes";
+console.log(user["name"]);
