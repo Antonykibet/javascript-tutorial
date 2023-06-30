6192349e2b2f3f89e7961a28adb4b8e1c5f9c14b
Antony Kibet <antony.kibet@metropol.local>
2023-06-30 17:03:17 +0300

    callback, promises

--- today.js
+++ today.js
@@ -1059,4 +1059,86 @@ console.lo
     
 // }
 
-*/
+
+
+//CALLBACK
+
+let data="rainy";
+function displayWeather(data){
+    if(data=='sunny'){console.log(data)}
+    if(data=='rainy'){console.log(data)}
+}
+
+function getWeather(callback){
+    setTimeout(function(){
+        callback(data);
+    },3000);
+}
+ 
+getWeather(displayWeather);
+
+
+//PROMISES
+Returning promises
+A handler, used in .then(handler) may create and return a promise.
+
+In that case further handlers wait until it settles, and then get its result.
+
+Thenables
+To be precise, a handler may return not exactly a promise, but a so-called “thenable” object – an arbitrary object that has a method .then. It will be treated the same way as a promise.
+
+The idea is that 3rd-party libraries may implement “promise-compatible” objects of their own. They can have an extended set of methods, but also be compatible with native promises, because they implement .then.
+
+Here’s an example of a thenable object:
+
+class Thenable {
+  constructor(num) {
+    this.num = num;
+  }
+  then(resolve, reject) {
+    alert(resolve); // function() { native code }
+    // resolve with this.num*2 after the 1 second
+    setTimeout(() => resolve(this.num * 2), 1000); // (**)
+  }
+}
+
+new Promise(resolve => resolve(1))
+  .then(result => {
+    return new Thenable(result); // (*)
+  })
+  .then(alert); // shows 2 after 1000ms
+JavaScript checks the object returned by the .then handler in line (*): if it has a callable method named then, then it calls that method providing native functions resolve, reject as arguments (similar to an executor) and waits until one of them is called. In the example above resolve(2) is called after 1 second (**). Then the result is passed further down the chain.
+
+This feature allows us to integrate custom objects with promise chains without having to inherit from Promise.
+We’ll use the fetch method to load the information about the user from the remote server. It has a lot of optional parameters covered in separate chapters, but the basic syntax is quite simple:
+
+let promise = fetch(url);
+This makes a network request to the url and returns a promise. The promise resolves with a response object when the remote server responds with headers, but before the full response is downloaded.
+
+To read the full response, we should call the method response.text(): it returns a promise that resolves when the full text is downloaded from the remote server, with that text as a result.
+
+The code below makes a request to user.json and loads its text from the server:
+
+fetch('/article/promise-chaining/user.json')
+  // .then below runs when the remote server responds
+  .then(function(response) {
+    // response.text() returns a new promise that resolves with the full response text
+    // when it loads
+    return response.text();
+  })
+  .then(function(text) {
+    // ...and here's the content of the remote file
+    alert(text); // {"name": "iliakan", "isAdmin": true}
+  });
+The response object returned from fetch also includes the method response.json() that reads the remote data and parses it as JSON. In our case that’s even more convenient, so let’s switch to it.
+
+We’ll also use arrow functions for brevity:
+
+// same as above, but response.json() parses the remote content as JSON
+fetch('/article/promise-chaining/user.json')
+  .then(response => response.json())
+  .then(user => alert(user.name)); // iliakan, got user name
+
+
+  As a good practice, an asynchronous action should always return a promise. That makes it possible to plan actions after it; even if we don’t plan to extend the chain now, we may need it later.
+  */
